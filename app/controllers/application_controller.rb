d576c365a0b549650b272a078cfe80d11d3a1eb8
jaamanu <jaamanu@gmail.com>
2023-02-18 11:56:47 +0200

    Add devise for aunthentication and email validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    User.first
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email name photo])
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:name, :email, :photo, :bio, :password, :password_confirmation, :current_password)
+    end
   end
 end
